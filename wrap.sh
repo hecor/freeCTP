c20186d0e7c0e8dd6ca6062372a6cf276851cb33
hecor <husthecor@gmail.com>
2014-02-28 12:13:02 +0800

    delete some redundant files

--- wrap.sh
+++
@@ -1,6 +0,0 @@
-swig -c++ -python pzyctp.swig
-gcc -I/home/hecor/programs/include/python2.7 -fPIC -c pzyctp_wrap.cxx
-gcc -I/home/hecor/programs/include/python2.7 -fPIC -c trader.cpp
-gcc -I/home/hecor/programs/include/python2.7 -fPIC -c datafeeder.cpp
-g++ -shared pzyctp_wrap.o trader.o datafeeder.o -o _pzyctp.so -lthosttraderapiSSE -lthostmduserapiSSE -llevel2userapi -L./api
-
